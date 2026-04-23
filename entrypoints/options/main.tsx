ad831834e2a22e23e6bd6a294c58d22c63abaf20
Oscar Rodríguez <136043524+khar20@users.noreply.github.com>
2026-04-23 17:58:02 -0500

    feat: add initial global styling

--- entrypoints/options/main.tsx
+++ entrypoints/options/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import OptionsPage from './Options.tsx';
-import './style.css';
+import '../../styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- entrypoints/welcome/main.tsx
+++ entrypoints/welcome/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WelcomePage from './Welcome.tsx';
-import './style.css';
+import '../../styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
